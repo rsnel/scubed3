6b1695e05ddfc58a96ee2e9805ca0d2505e96124
Rik Snel <rik@snel.it>
2008-06-01 13:37:25 +0000

    update

--- version.sh
+++ version.sh
@@ -7,4 +7,8 @@ test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut
 test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
 test $svn_revision || svn_revision=UNKNOWN
 
-echo SVN-r${svn_revision}
+dirty=""
+if [ "`svn status | wc -l`" != "0" ]; then
+	dirty="dirty"
+fi
+echo SVN-r${svn_revision}${dirty}

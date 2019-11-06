e9fb52416ba66924afb4a6e18521e816ae0d39b4
Rik Snel <rik@snel.it>
2019-11-06 08:17:52 +0100

    update version.sh to use git

--- version.sh
+++ version.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
 
-if [ ! -d .svn/ ]; then
+if [ ! -d .git/ ]; then
 	echo UNKNOWN
 	exit
 fi
-test "$1" && extra="-$1"
 
-svn_revision=`LC_ALL=C svn info 2> /dev/null | grep Revision | cut -d' ' -f2`
-test $svn_revision || svn_revision=`grep revision .svn/entries 2>/dev/null | cut -d '"' -f2`
-test $svn_revision || svn_revision=`sed -n -e '/^dir$/{n;p;q;}' .svn/entries 2>/dev/null`
-test $svn_revision || svn_revision=UNKNOWN
-
-dirty=""
-if [ "`svn status | wc -l`" != "0" ]; then
-	dirty="dirty"
-fi
-echo SVN-r${svn_revision}${dirty}
+git describe
